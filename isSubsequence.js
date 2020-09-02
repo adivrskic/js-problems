8db0a2a07b3226e61cf34e7e1bc47a5a87259b8b
Adi Vrskic <35346212+adivrskic@users.noreply.github.com>
2020-09-02 11:23:54 -0400

    Create isSubsequence.js

---
+++ isSubsequence.js
@@ -0,0 +1,19 @@
+function isSubsequence(str1, str2) {
+  let s1 = 0;
+  let s2 = 0;
+  let count = 0;
+
+  while (s2 !== str2.legnth) {
+    if (str1[s1] === str2[s2]) {
+      s1++;
+      s2++;
+      count++;
+
+      if (count === str1.length) return true;
+    } else if (str1[s1] !== str2[s2]) {
+      s2++;
+    }
+  }
+
+  return false;
+}

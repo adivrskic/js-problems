35ce294d94047c7ff61068f86bd42382a47f7648
Adi Vrskic <35346212+adivrskic@users.noreply.github.com>
2020-09-02 11:25:31 -0400

    Update isSubsequence.js

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,3 +1,6 @@
+// a function that takes in 2 strings and checks to see if the characters in the first string
+// form a subsequence of the characters in the second string
+
 function isSubsequence(str1, str2) {
   let s1 = 0;
   let s2 = 0;
